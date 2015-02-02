a3d59797f895b24305633c5fdc9ef41625f087cb
nimdanet <jj.nimdnet@gmail.com>
2015-02-02 20:29:31 -0200

    First steps: - Included all menu assets - Firsts menu interactions

---
+++ Assets/Scripts/Menu/MainMenuScreen.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScreen : MonoBehaviour 
+{
+	private enum Screen
+	{
+		None,
+		Main,
+		Options,
+		Credits,
+		Achievements,
+	}
+
+	private Screen currentScreen = Screen.None;
+
+	public GameObject placa;
+
+	public GameObject main;
+	public GameObject options;
+	public GameObject credits;
+	public GameObject achievements;
+
+	void Update()
+	{
+		if(currentScreen != Screen.None) return;
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			placa.SetActive(true);
+			currentScreen = Screen.Main;
+		}
+	}
+
+	public void OnPlayClicked()
+	{
+
+	}
+
+	public void OnOptionsClicked()
+	{
+
+	}
+
+	public void OnCreditsClicked()
+	{
+
+	}
+
+	public void OnAchievementsClicked()
+	{
+
+	}
+}
